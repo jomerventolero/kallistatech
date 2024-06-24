d6926eb12dc2fb90baff2f0f7c3b3a8140bf46d2
jomerventolero <hiroventolero@gmail.com>
2024-06-24 20:52:30 +0800

    Fix for ChatWidget and Google Tag Manager

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -7,52 +7,34 @@ import ScrollToTop from "@/components/ScrollToTop";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import { useEffect } from "react";
+
 const inter = Inter({ subsets: ["latin"] });
 
 import ToasterContext from "../context/ToastContext";
-
-import { Head } from "next/document";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  useEffect(() => {
+    // Add Tawk.to script
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = 'https://embed.tawk.to/6495ac5294cf5d49dc5f7343/1h3kai8it';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.body.appendChild(script);
+
+    return () => {
+      // Cleanup script when component unmounts
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-          {/* Google Tag Manager */}
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-4TEVFTFZ4D"
-          ></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', 'G-4TEVFTFZ4D');
-              `,
-            }}
-          />
-          {/* End Google Tag Manager */}
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-                (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/6495ac5294cf5d49dc5f7343/1h3kai8it';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-                })();
-              `,
-            }}
-          />
-      </Head>
       <body className={`dark:bg-black ${inter.className}`}>
         <ThemeProvider
           enableSystem={false}

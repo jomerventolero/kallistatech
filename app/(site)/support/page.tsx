4ce360e8a99feffa5e4ce244b5f5c63043f39182
jomerventolero <hiroventolero@gmail.com>
2025-01-21 01:53:32 +0800

    Fix: Meta SEO for Support Page

--- app/(site)/support/page.tsx
+++ app/(site)/support/page.tsx
@@ -3,8 +3,8 @@ import Contact from "@/components/Contact";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Support Page - Solid SaaS Boilerplate",
-  description: "This is Support page for Solid Pro",
+  title: "Contact us - Kallista Tech Solutions",
+  description: "Send us a message. Kallista Tech Solutions",
   // other metadata
 };
 

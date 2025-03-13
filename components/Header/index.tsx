c7284ef18de7980b855a5648eeef48201e1e1011
jomerventolero <hiroventolero@gmail.com>
2025-03-13 16:39:39 +0800

    Hotfix: Dark Theme

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -152,7 +152,7 @@ const Header = () => {
           </nav>
 
           <div className="mt-7 flex items-center gap-6 xl:mt-0">
-            <ThemeToggler />
+            {/*<ThemeToggler />*/}
             <Link
               href="/support"
               className="flex items-center justify-center rounded-full bg-primary px-7.5 py-2.5 text-regular text-white duration-300 ease-in-out hover:bg-primaryho"
